16d25a3feb36b956d4f077d52f881c10090e8b38
fabnight <58448176+fabnight@users.noreply.github.com>
2021-04-09 00:20:42 +0200

    initialisation projet

---
+++ src/main/java/com/safetynet/safetynetrescuesystem/SafetyNetRescueSystemApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.safetynetrescuesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetRescueSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyNetRescueSystemApplication.class, args);
+	}
+
+}
