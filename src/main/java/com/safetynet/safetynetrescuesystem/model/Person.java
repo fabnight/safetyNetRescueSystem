70cb230973dda9197feb4ef9d2063720e09b793f
fabnight <58448176+fabnight@users.noreply.github.com>
2021-04-09 21:14:25 +0200

    Add personController

---
+++ src/main/java/com/safetynet/safetynetrescuesystem/model/Person.java
@@ -0,0 +1,90 @@
+package com.safetynet.safetynetrescuesystem.model;
+
+public class Person {
+
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private int zip;
+	private String phone;
+	private String email;
+
+	// constructeur par défaut
+	public Person() {
+	}
+
+	// constructeur pour les tests
+	public Person(String firstName, String lastName, String address, String city, String phone, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		//this.zip = zip;
+		this.phone = phone;
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	 @Override
+	    public String toString(){  
+	        return "Person{"+
+	        "firstName=" + firstName + 
+	        ", lastname='"+ lastName + '\'' + 
+	        ", address=" + address+ '}';
+	    }
+}
---
+++ src/main/java/com/safetynet/safetynetrescuesystem/web/controller/PersonController.java
@@ -0,0 +1,25 @@
+package com.safetynet.safetynetrescuesystem.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.safetynet.safetynetrescuesystem.model.Person;
+
+@RestController
+public class PersonController {
+@RequestMapping(value="/Persons",method=RequestMethod.GET)
+public String listePersons() {
+	return "un exemple de person";
+}
+
+//Récupérer une personne par son Prénom/Nom
+
+@GetMapping(value="/Persons/{firstName}/{lastName}")
+public Person afficherUnePerson(@PathVariable String firstName, String lastName) {
+  Person person=new Person(firstName, lastName, new String("5 rue Jean Moquet"),new String("Hambourg"), new String("44 55 55 66"),new String("ert@gmail.com")) ;
+  return person;
+}
+}
