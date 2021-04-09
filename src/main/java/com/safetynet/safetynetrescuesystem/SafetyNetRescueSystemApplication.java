2c143f186fd7ff8b7e6d3822a983f012872f9b85
fabnight <58448176+fabnight@users.noreply.github.com>
2021-04-09 21:10:14 +0200

    creation of person controller

--- src/main/java/com/safetynet/safetynetrescuesystem/SafetyNetRescueSystemApplication.java
+++ src/main/java/com/safetynet/safetynetrescuesystem/SafetyNetRescueSystemApplication.java
@@ -1,13 +1,18 @@
 package com.safetynet.safetynetrescuesystem;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SafetyNetRescueSystemApplication {
+public class SafetyNetRescueSystemApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetRescueSystemApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!");
+	}
 }
